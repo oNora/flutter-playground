280ef652e0e42c82384f96bacadc1cc930d7109b
oNora <e.dimchevska@gmail.com>
2022-03-16 21:17:34 +0200

    firts project form the Flutter course

---
+++ flutter_basics/lib/result.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback resetHandle;
+
+  Result(this.resultScore, this.resetHandle);
+
+  String get resultPhrase {
+    var resultText = 'You did it';
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are strange!';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandle,
+            child: Text('Restart the Quiz'),
+            textColor: Colors.blue,
+          )
+        ],
+      ),
+    );
+  }
+}
