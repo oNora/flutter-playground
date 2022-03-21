af89088ff9e04c5da3416ce89f3ace283dda1ba3
oNora <e.dimchevska@gmail.com>
2022-03-21 16:46:12 +0200

    use new kind of widget for a list items

--- expense_planner/lib/widgets/transacrio_list.dart
+++ expense_planner/lib/widgets/transacrio_list.dart
@@ -32,46 +32,69 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (BuildContext context, int index) {
+                // return Card(
+                //   child: Row(
+                //     children: <Widget>[
+                //       Container(
+                //         margin: EdgeInsets.symmetric(
+                //           vertical: 10,
+                //           horizontal: 15,
+                //         ),
+                //         decoration: BoxDecoration(
+                //           border: Border.all(
+                //             color: Theme.of(context).primaryColor,
+                //             width: 2,
+                //           ),
+                //         ),
+                //         padding: EdgeInsets.all(10),
+                //         child: Text(
+                //           '\$${userTransactions[index].amount.toStringAsFixed(2)}',
+                //           style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             fontSize: 20,
+                //             color: Theme.of(context).primaryColor,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: <Widget>[
+                //           Text(
+                //             userTransactions[index].title,
+                //             style: Theme.of(context).textTheme.titleLarge,
+                //           ),
+                //           Text(
+                //             DateFormat.yMMMMd()
+                //                 .format(userTransactions[index].date),
+                //             // DateFormat('dd-mm-yyyy').format(tx.date),
+                //             style: TextStyle(color: Colors.grey),
+                //           ),
+                //         ],
+                //       )
+                //     ],
+                //   ),
+                // );
+
+                // alternative to the above is bild in Flutter widget:
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${userTransactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                            child: Text('\$${userTransactions[index].amount}')),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            userTransactions[index].title,
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                          Text(
-                            DateFormat.yMMMMd()
-                                .format(userTransactions[index].date),
-                            // DateFormat('dd-mm-yyyy').format(tx.date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      userTransactions[index].title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(userTransactions[index].date),
+                    ),
                   ),
                 );
               },
