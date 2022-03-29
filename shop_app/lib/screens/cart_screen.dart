7b9ab6eed0b7f05ac6e754a8e4717d754d9ffae0
oNora <e.dimchevska@gmail.com>
2022-03-29 13:32:01 +0300

    add slipe efect for deleting

--- shop_app/lib/screens/cart_screen.dart
+++ shop_app/lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -43,6 +44,19 @@ class CartScreen extends StatelessWidget {
                     ]),
               ),
             ),
+            SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: ((context, index) => CartItem(
+                      cart.items.values.toList()[index].id,
+                      cart.items.keys.toList()[index],
+                      cart.items.values.toList()[index].price,
+                      cart.items.values.toList()[index].quantity,
+                      cart.items.values.toList()[index].title,
+                    )),
+                itemCount: cart.itemCount,
+              ),
+            )
           ],
         ));
   }
