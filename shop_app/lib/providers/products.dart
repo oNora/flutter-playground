2b603013336d5e9bf5f3883065fb1a4df2b007b1
oNora <e.dimchevska@gmail.com>
2022-03-31 15:22:08 +0300

    connection to Firebase &  first post request

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -65,16 +67,35 @@ class Products with ChangeNotifier {
     return _items.where((productId) => productId.isFavorite).toList();
   }
 
-  void addProduct(Product product) {
-    final newProduct = Product(
-        id: DateTime.now().toString(),
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl);
-    _items.add(newProduct);
-    // _items.insert(0, newProduct); // at the begind of the list
-    notifyListeners();
+  Future<void> addProduct(Product product) {
+    Uri url = Uri.parse(
+        'https://flutter-playground-9144c-default-rtdb.europe-west1.firebasedatabase.app/products.json');
+    return http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+      final newProduct = Product(
+          id: json.decode(response.body)['name'],
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl);
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); // at the begind of the list
+      notifyListeners();
+    }).catchError((error) {
+      print('error from post request: ADD/EDIT product');
+      print(error);
+      throw error;
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
