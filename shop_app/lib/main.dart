c06a3c088120d92a48a299757b6a5480176ebabd
oNora <e.dimchevska@gmail.com>
2022-04-05 15:36:25 +0300

    add auto login

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -12,6 +12,7 @@ import './screens/orders_screen.dart';
 import './screens/user_products_scree.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
+import './screens/splash_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -52,7 +53,19 @@ class MyApp extends StatelessWidget {
               accentColor: Colors.deepOrange,
               fontFamily: 'Lato',
             ),
-            home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+            home: auth.isAuth
+                ? ProductsOverviewScreen()
+                : FutureBuilder(
+                    future: auth.tryAutoLogin(),
+                    builder: (
+                      ctx,
+                      authResultSnapshot,
+                    ) =>
+                        authResultSnapshot.connectionState ==
+                                ConnectionState.waiting
+                            ? SplashScreen()
+                            : AuthScreen(),
+                  ),
             routes: {
               ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
               CartScreen.routeName: (ctx) => CartScreen(),
