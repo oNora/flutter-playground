df4b9fd492d4610bc8c7a0c99f3bd13a80c3e935
oNora <e.dimchevska@gmail.com>
2022-04-08 12:54:54 +0300

    fix type error

--- shop_app/lib/screens/auth_screen.dart
+++ shop_app/lib/screens/auth_screen.dart
@@ -255,6 +255,7 @@ class _AuthCardState extends State<AuthCard>
                       if (value.isEmpty || value.length < 5) {
                         return 'Password is too short!';
                       }
+                      return null;
                     },
                     onSaved: (value) {
                       _authData['password'] = value;
@@ -271,6 +272,7 @@ class _AuthCardState extends State<AuthCard>
                               if (value != _passwordController.text) {
                                 return 'Passwords do not match!';
                               }
+                              return null;
                             }
                           : null,
                     ),
